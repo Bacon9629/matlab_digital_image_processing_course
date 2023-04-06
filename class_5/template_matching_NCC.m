ecb8868d40336859b7f3fca8b79a6a3dc4a9a69a
Bacon9629 <qwe43213652@gmail.com>
2023-04-06 23:19:10 +0800

    0406-2

--- class_5/template_matching_NCC.m
+++ class_5/template_matching_NCC.m
@@ -1,10 +1,12 @@
 clc;clear;close all;
-I = imread("eye.jpg");
+% I = imread("eye.jpg");
+I = imread("onion.png");
 I = rgb2gray(I);
 figure
 imshow(I);
-w=imread('eye_mask_2.jpg');
-w = rgb2gray(w);
+% w=imread('eye_mask_2.jpg');
+w=I(30:50, 50:70);
+% w = rgb2gray(w);
 figure
 imshow(w)
 
--- class_6/imcorr.m
+++ class_6/imcorr.m
@@ -3,8 +3,10 @@ function [Icorr, iMax, jMax, c] = imcorr(I, w)
 Icorr = zeros(m-m0+1, n-n0+1);
 vecW = double(w(:));
 normW = norm(vecW);
-for ii = 1:n-m0+1
+for ii = 1:m-m0+1
     for jj = 1:n-n0+1
+%         display(ii);
+%         display(jj);
         subMat = I(ii:ii+m0-1, jj:jj+n0-1);
         vec = double(subMat(:));
         Icorr(ii, jj) = vec'*vecW/(norm(vec)*normW+eps);
